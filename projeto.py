bd72b6d6b17ba9ab111d1e900b0f9d42c35cc068
Gabriel <gabrieldesousa1255@gmail.com>
2025-06-04 17:38:23 -0300

    estrutura

--- projeto.py
+++ projeto.py
@@ -5,15 +5,36 @@ page.title("Calculadora")
 page.geometry("400x600")
 page.resizable(False, False)
 
+display= tk.StringVar()
+display_calc= tk.Entry(
+    page,
+    textvariable=display,
+    borderwidth= 10,
+    font= ("Arial", 24),
+    relief= "sunken",
+    justify= "right",
+    state= "readonly")
 botoes= [
-    ["Calculadora"]    
+    ["C", "←", "%", "÷"],
+    ["7", "8", "9", "×"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "="]
 ]
 for line in botoes:
-    lineframe=tk.Frame(page)
-    lineframe.pack(expand=True, fill="x")
-    for botao in line:
-        botao=tk.Button(
-            lineframe, text=botao, font=("arial", 12)
-        )
-    botao.pack(expand=True, fill="both")
+    line_frame= tk.Frame(page)
+    line_frame.pack(expand=True, fill="both")
+    for caractere in line:
+        if caractere == "":
+            tk.Label(line_frame).pack(side= "left",
+            expand= True,
+            fill= "both")
+
+        tk.Button(
+        line_frame,
+        text= caractere,
+        ).pack(side= "left",
+        expand= True,
+        fill= "both")
+
 page.mainloop()
